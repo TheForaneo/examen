de46c3451fb97afcdea8a5b87ac97cee907d7064
TheForaneo <15171064@itculiacan.edu.mx>
2020-05-13 12:22:43 -0600

    KA creacion de la carpeta Models y el modelo Aerolinea

---
+++ Models/Aerolinea.cs
@@ -0,0 +1,8 @@
+namespace examen.Models{
+    public class Aerolinea{
+        public int Id {get; set;}
+        public string Nombre {get; set;}
+        public string PaisOrigen {get; set;}
+        public int Calificacion {get; set;}
+    }
+}
